e0623223b0759a8692445812807e58c3645fb976
darshil <dpatel0698@gmail.com>
2024-07-31 15:03:10 -0400

    Serializing and deserializing Huffman Tree implemented

--- HuffmanCoder.py
+++ HuffmanCoder.py
@@ -1,5 +1,7 @@
 # Defines and implements nodes and tree for Huffman Coding
 from __future__ import annotations
+
+import collections
 import heapq
 
 
@@ -38,11 +40,74 @@ class HuffmanTree:
     def get_root(self):
         return self.root
 
+    def set_root(self, root):
+        self.root = root
+
     def weight(self) -> int:
         return self.root.get_weight()
 
     def __lt__(self, other: HuffmanTree):
         return self.weight() < other.weight()
+
+    @staticmethod
+    def serialize(root):
+        """
+        Converts the Huffman Tree into a string form that can be saved to a file
+        :return: string encoding of the Huffman Tree
+        """
+        out = []
+        q = collections.deque([root])
+        while q:
+            node = q.pop()
+            if node:
+                if node.is_leaf():
+                    out.append(str(node.get_weight()) + "|" + node.get_value())
+                else:
+                    out.append(str(node.get_weight()))
+                q.appendleft(node.get_left())
+                q.appendleft(node.get_right())
+            else:
+                out.append("N")
+        return ",".join(out)
+
+    @staticmethod
+    def deserialize_node_encoding(i, flat_data):
+        weight, char = None, None
+        if "|" in flat_data[i]:
+            contents = flat_data[i].split("|")
+            weight = int(contents[0])
+            if len(contents) == 3:
+                char = "|"
+            else:
+                char = "," if contents[1] == "" else contents[1]
+        else:
+            weight = int(flat_data[i])
+        return HuffmanNode(weight=weight, char=char)
+
+    @staticmethod
+    def deserialize(data):
+        """
+        Converts a string encoding into a Huffman Tree
+        :return: Huffman Tree
+        """
+        if not data:
+            return
+        flat_data = data.split(',')
+        root = HuffmanNode(weight=int(flat_data[0]))
+        queue = collections.deque([root])
+        i = 1
+        # when you pop a node, its children will be at i and i+1
+        while queue:
+            node = queue.pop()
+            if i < len(flat_data) and flat_data[i] != "N":
+                node.left = HuffmanTree.deserialize_node_encoding(i, flat_data)
+                queue.appendleft(node.left)
+            i += 1
+            if i < len(flat_data) and flat_data[i] != "N":
+                node.right = HuffmanTree.deserialize_node_encoding(i, flat_data)
+                queue.appendleft(node.right)
+            i += 1
+        return root
 
     @staticmethod
     def buildTree(huff_heap: list[HuffmanTree]) -> HuffmanTree:
@@ -65,6 +130,17 @@ class HuffmanTree:
         table.update(self.__get_code_table(node.get_left(), True, code + "0"))
         table.update(self.__get_code_table(node.get_right(), False, code + "1"))
         return table
+
+    def search_char(self, code):
+        """
+        Searches for a code in the tree and returns the character.
+        Moving left in the tree is "0" and right is "1".
+        :param code: a binary code
+        :return: a character
+        """
+        pass
+
+
 
 
 
---
+++ huffmancompress.py
@@ -0,0 +1,40 @@
+import sys
+from collections import Counter
+import heapq
+from HuffmanCoder import HuffmanTree
+
+
+def get_char_frequencies(text):
+    frequencies = Counter(text)
+    return frequencies
+
+
+def compress_text(text, code_table):
+    output = ""
+    for char in text:
+        output += code_table[char]
+    return output
+
+
+if __name__ == "__main__":
+    try:
+        if sys.argv[1] == "-e":
+            with open(sys.argv[2], "r", encoding="utf-8") as file:
+                file_text = file.read()
+                char_counts = get_char_frequencies(file_text)
+                min_heap = []
+                for char, val in char_counts.items():
+                    heapq.heappush(min_heap, HuffmanTree(char, val))
+                # Generate Tree and Prefix Code Table
+                tree = HuffmanTree.buildTree(min_heap)
+                code_table = tree.generate_code_table()
+                # Compress the given file
+                with open("compressed_" + sys.argv[2], "w") as out_file:
+                    tree_encoding = HuffmanTree.serialize(tree.get_root())
+                    out_file.write(tree_encoding + "\n")
+                    out_file.write(compress_text(file_text, code_table))
+                    out_file.close()
+                file.close()
+
+    except FileNotFoundError:
+        print(f"{sys.argv[1]} not found")
